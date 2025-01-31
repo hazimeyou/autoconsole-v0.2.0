2db8130cc28e58f556ee4424a7f34d9bafd6282e
はじめよう <hazimeyou0922@gmail.com>
2025-01-31 22:18:47 +0900

    プロジェクト ファイルを追加します。

---
+++ autoconsole-v0.2.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // import変数でコンソールからの入力を受け取る
+        Console.Write("Enter your input: ");
+        string import = Console.ReadLine();
+
+        // output変数にコンソールへの出力を格納
+        string output = ProcessInput(import);
+
+        // 結果を表示
+        Console.WriteLine("Output: " + output);
+    }
+
+    static string ProcessInput(string input)
+    {
+        // 入力に対して何か処理を行う
+        // ここでは例として入力文字列を逆順にする
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
